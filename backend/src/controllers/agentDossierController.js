18a516b4cd0574012e1cc9a2bea2e90482d62150
Ginks <pissabruney@hotmail.com>
2026-08-23 08:59:58 +0200

    Expose complete GenOS agent dossiers

---
+++ backend/src/controllers/agentDossierController.js
@@ -0,0 +1,16 @@
+const { getDatabase } = require('../db');
+const { loadAgentDossier } = require('../services/agentDossierService');
+
+async function getAgentDossier(req, res, next) {
+  try {
+    const dossier = await loadAgentDossier(await getDatabase(), req.params.id);
+    if (!dossier) {
+      return res.status(404).json({ error: { code: 'NOT_FOUND', message: `Agent ${req.params.id} not found` } });
+    }
+    res.json(dossier);
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = { getAgentDossier };
