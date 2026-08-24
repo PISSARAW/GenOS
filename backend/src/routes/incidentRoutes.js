3616df08b9f88ee88da6634c8135a33a0f71363f
Ginks <pissabruney@hotmail.com>
2026-08-24 13:10:02 +0200

    Add optional pagination to fleet, alerts and workspace lists

--- backend/src/routes/incidentRoutes.js
+++ backend/src/routes/incidentRoutes.js
@@ -6,8 +6,9 @@ const express = require('express');
 const router = express.Router();
 const incidentController = require('../controllers/incidentController');
 const { requirePermission } = require('../middleware/auth');
+const { paginateList } = require('../controllers/listPagination');
 
-router.get('/alerts', incidentController.getAlerts);
+router.get('/alerts', paginateList(incidentController.getAlerts));
 router.get('/incidents', incidentController.getIncidents);
 router.post('/incidents/replay', incidentController.replayIncident);
 router.post('/tasks/:id/kill', requirePermission('emergency_kill'), incidentController.killTask);
