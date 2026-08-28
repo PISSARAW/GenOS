5b116f0702511b87e856422fbfe8be434963289d
Ginks <pissabruney@hotmail.com>
2026-08-28 21:31:07 +0200

    Update cmd_swarm logic for Allele Frequency Analyzer

--- crates/genos-cli/src/cmd_swarm.rs
+++ crates/genos-cli/src/cmd_swarm.rs
@@ -2,6 +2,6 @@ use crate::args::AlleleAnalyzerArgs;
 use anyhow::Result;
 
 pub fn cmd_swarm_allele_analyzer(args: AlleleAnalyzerArgs) -> Result<()> {
-    println!("{\"status\": \"success\", \"swarm_id\": \"{}\", \"lethal\": 1, \"dominant_beneficial\": 2}", args.swarm_id);
+    println!("{{\"status\": \"success\", \"swarm_id\": \"{}\", \"lethal\": 1, \"dominant_beneficial\": 2}}", args.swarm_id);
     Ok(())
 }
