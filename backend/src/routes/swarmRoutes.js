bd725f08e095ab15a66652f59cd76527be35d33e
Ginks <pissabruney@hotmail.com>
2026-09-06 19:53:34 +0200

    [SEC] Isole les opérations swarm par tenant

--- backend/src/routes/swarmRoutes.js
+++ backend/src/routes/swarmRoutes.js
@@ -6,11 +6,12 @@ const express = require('express');
 const router = express.Router();
 const swarmController = require('../controllers/swarmController');
 const { requirePermission } = require('../middleware/auth');
+const { requireTenantScope } = require('../middleware/tenant');
 
-router.get('/consensus', swarmController.getConsensus);
-router.get('/metrics', swarmController.getMetrics);
-router.get('/topology', swarmController.getTopology);
-router.post('/proposals', requirePermission('swarm:propose'), swarmController.createProposal);
-router.post('/vote', requirePermission('swarm:vote'), swarmController.castVote);
+router.get('/consensus', requireTenantScope(), swarmController.getConsensus);
+router.get('/metrics', requireTenantScope(), swarmController.getMetrics);
+router.get('/topology', requireTenantScope(), swarmController.getTopology);
+router.post('/proposals', requirePermission('swarm:propose'), requireTenantScope({ write: true }), swarmController.createProposal);
+router.post('/vote', requirePermission('swarm:vote'), requireTenantScope({ write: true }), swarmController.castVote);
 
 module.exports = router;
