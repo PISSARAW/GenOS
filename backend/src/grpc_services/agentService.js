1f715eff5bd1b8d186b965e20bfdb42646db264e
Ginks <pissabruney@hotmail.com>
2026-09-06 20:19:37 +0200

    [FIX] Valide les contrats de mission gRPC

--- backend/src/grpc_services/agentService.js
+++ backend/src/grpc_services/agentService.js
@@ -1,4 +1,5 @@
 const runtimeAdapter = require('../services/agentRuntimeAdapter');
+const grpc = require('@grpc/grpc-js');
 
 module.exports = {
   Ping: (call, callback) => callback(null, { status: "Service Agent is alive via gRPC!" }),
@@ -16,18 +17,18 @@ module.exports = {
         workspaceIsolation: mission.workspace_isolation,
         agentType: mission.agent_type,
         executionMode: mission.execution_mode,
-        strategyContract: parseJson(mission.strategy_contract_json, {}),
+        strategyContract: parseJson(mission.strategy_contract_json, {}, 'strategy_contract_json'),
         orchestratorAgentId: mission.orchestrator_agent_id,
-        autonomyPlan: parseJson(mission.autonomy_plan_json, {}),
-        toolLease: parseJson(mission.tool_lease_json, []),
-        genosCapsule: parseJson(mission.genos_capsule_json, {}),
-        executionPolicy: parseJson(mission.execution_policy_json, {}),
-        executionBudget: parseJson(mission.execution_budget_json, {}),
+        autonomyPlan: parseJson(mission.autonomy_plan_json, {}, 'autonomy_plan_json'),
+        toolLease: parseJson(mission.tool_lease_json, [], 'tool_lease_json'),
+        genosCapsule: parseJson(mission.genos_capsule_json, {}, 'genos_capsule_json'),
+        executionPolicy: parseJson(mission.execution_policy_json, {}, 'execution_policy_json'),
+        executionBudget: parseJson(mission.execution_budget_json, {}, 'execution_budget_json'),
         nameMeaning: mission.name_meaning
       });
       callback(null, { success: true, message: `Mission for agent ${mission.agent_id} started` });
     } catch (err) {
-      callback(null, { success: false, message: err.message });
+      callback({ code: err.code === 'INVALID_MISSION_JSON' ? grpc.status.INVALID_ARGUMENT : grpc.status.INTERNAL, message: err.message });
     }
   },
 
@@ -38,6 +39,13 @@ module.exports = {
   }
 };
 
-function parseJson(value, fallback) {
-  try { return value ? JSON.parse(value) : fallback; } catch (_) { return fallback; }
+function parseJson(value, fallback, fieldName) {
+  if (!value) return fallback;
+  try {
+    return JSON.parse(value);
+  } catch (_) {
+    const error = new Error(`${fieldName} must be valid JSON.`);
+    error.code = 'INVALID_MISSION_JSON';
+    throw error;
+  }
 }
