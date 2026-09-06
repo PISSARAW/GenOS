169e2dd1df6d22440ac908953d8bedecc2b5b33f
Ginks <pissabruney@hotmail.com>
2026-09-06 19:51:59 +0200

    [FIX] Fiabilise les wrappers CLI Windows

--- g.cmd
+++ g.cmd
@@ -1,2 +1,12 @@
 @echo off
-cargo run -q -p genos-simple-cli -- %*
+where cargo >nul 2>&1
+if errorlevel 1 (
+	set "CARGO=%USERPROFILE%\.cargo\bin\cargo.exe"
+	if not exist "%CARGO%" (
+		echo Cargo introuvable. Installez Rust ou ajoutez cargo.exe au PATH.
+		exit /b 1
+	)
+) else (
+	set "CARGO=cargo"
+)
+"%CARGO%" run -q -p genos-simple-cli -- %*
--- g.ps1
+++ g.ps1
@@ -1,1 +1,8 @@
-cargo run -q -p genos-simple-cli -- $args
+$cargo = (Get-Command cargo -ErrorAction SilentlyContinue).Source
+if (-not $cargo) {
+	$cargo = Join-Path $HOME '.cargo\bin\cargo.exe'
+}
+if (-not (Test-Path $cargo)) {
+	throw "Cargo introuvable. Installez Rust ou ajoutez cargo.exe au PATH."
+}
+& $cargo run -q -p genos-simple-cli -- $args
